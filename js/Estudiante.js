f5b82d9372584c9a8e9a442e224504948b19aa27
MarioKoko777 <lilcocomusicoficial@gmail.com>
2025-05-02 13:34:57 +0200

    Update Estudiante.js

--- js/Estudiante.js
+++ js/Estudiante.js
@@ -1,4 +1,3 @@
-
 export class Estudiante {
   constructor(id, nombre, edad, direccion) {
     this.id = id;
@@ -8,14 +7,12 @@ export class Estudiante {
     this.matriculas = new Map();
     this.calificaciones = new Map();
   }
-
   agregarCalificacion(idAsignatura, nota) {
     if (!this.calificaciones.has(idAsignatura)) {
       this.calificaciones.set(idAsignatura, []);
     }
     this.calificaciones.get(idAsignatura).push(nota);
   }
-
   obtenerPromedioGeneral() {
     let total = 0, count = 0;
     for (let notas of this.calificaciones.values()) {
@@ -24,7 +21,6 @@ export class Estudiante {
     }
     return count === 0 ? 0 : (total / count).toFixed(2);
   }
-
   obtenerPromedioPorAsignatura(idAsignatura) {
     let notas = this.calificaciones.get(idAsignatura) || [];
     let suma = notas.reduce((a, b) => a + b, 0);
